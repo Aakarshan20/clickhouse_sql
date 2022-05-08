205c5f2bd0e5af78497c3d9575ffc1512ed43bdc
abc <abc@test.com>
2022-05-08 13:45:00 +0800

    初始建表語句 觀察分表分庫功能

---
+++ demo.sql
@@ -0,0 +1,21 @@
+insert into t_stock values
+(101,'sku_002',2000.00,'2020-06-01 11:00:00'),
+(102,'sku_004',2500.00,'2020-06-01 12:00:00'),
+(103,'sku_002',2000.00,'2020-06-02 13:00:00'),
+(104,'sku_002',12000.00,'2020-06-03 13:00:00'),
+(105,'sku_002',600.00,'2020-06-04 12:00:00');
+
+CREATE TABLE t_stock
+(
+    `id` UInt32,
+    `sku_id` String,
+    `total_amount` Decimal(16, 2),
+    `create_time` Datetime
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMMDD(create_time)
+PRIMARY KEY id
+ORDER BY (id, sku_id);
+
+
+
